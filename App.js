3e21bf93bcb78c2657dd6f103c11ba2efc356fd5
Muhammad Mustafa Bilal Niazi <93573285+mustafa-bilal-niazi@users.noreply.github.com>
2023-04-28 12:02:09 +0500

    added slider for viewing list

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React, { useState, useEffect} from 'react'
-import { TouchableOpacity, StyleSheet, KeyboardAvoidingView, Text, TextInput, View, Image, ScrollView, ActivityIndicator } from 'react-native'
+import { TouchableOpacity, StyleSheet, KeyboardAvoidingView, Text, TextInput, View, Image, ScrollView, ActivityIndicator,Switch } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import * as Linking from 'expo-linking';
 import { StatusBar } from 'expo-status-bar';
+
 export default function App() {
 
+
+  const[Name,setName]=useState('')
+  const[Quantity,setQuantity]=useState('')
+  const[Unit,setUnit]=useState('')
+  const[dataRec,setDataRec]=useState([])
+  const [dataRec0,setDataRec0]=useState()
+  const [isLoading, setIsLoading] = useState(false);
+  const[fullList,setFullList] = useState(false)
+  const[listText,setListText] = useState('Show all items')
+
+
   const checkValid=()=> {
+   
     if(Name!==''){
+      setFullList(false)
       sendReq()
     }
     else {
@@ -13,11 +28,9 @@ export default function App() {
     }
   }
 
-  const[Name,setName]=useState('')
-  const[Quantity,setQuantity]=useState('')
-  const[Unit,setUnit]=useState('')
-  const[dataRec,setDataRec]=useState([])
-  const [isLoading, setIsLoading] = useState(false);
+  const checkValid2 = () => {
+    setFullList(!fullList);
+  };
 
   const sendReq = async() => {
     setIsLoading(true);
@@ -42,6 +55,9 @@ export default function App() {
         alert('No results found')
       }
       setDataRec(resJson.data)
+      console.log('--------------------------------------------------------------------')
+      console.log(resJson.data[0])
+      setDataRec0(resJson.data[0])
       console.log(resJson.data)
     }
     catch(error){
@@ -81,23 +97,51 @@ export default function App() {
       <TouchableOpacity
         onPress={() => checkValid()}>
         <Text style={{fontSize: 20,marginTop: 20, color: 'white', backgroundColor: 'black',paddingHorizontal: 20,paddingVertical: 8,borderRadius: 20, borderWidth: 2,borderColor: 'white'}}>
-          Send
+          Search
         </Text>
       </TouchableOpacity>
-        <ScrollView style= {styles.container2}>
-        {isLoading && <ActivityIndicator size={50} color="white" />}
-          {dataRec.map((item, index) => (
-          <View key={index} style={styles.item}>
-            <Text style={{fontWeight: 'bold',fontSize: 17, }}>ID: {item.id} - {item.name}</Text>
-            <Text >Quantity: {item.quantity}</Text>
-            <Text >Unit: {item.unit}</Text>
-            <Text >Price: {item.price.toFixed(2)}$</Text>
-            <Text style={{color: 'blue', fontWeight: 'bold'}}
-              onPress={() => Linking.openURL(`http:\\${item.website}`)}>
-              Website: {item.website}
-            </Text>
-          </View>
-          ))}
+      
+        <ScrollView 
+          style= {styles.container2}
+        >
+          {dataRec!=0 && <View style={{ alignItems: 'center' }}>
+            <View style={{flexDirection: 'row', backgroundColor: 'white', paddingHorizontal: 20,}}>
+              <Text style={{marginTop: 12,fontSize: 17,}}>Show all</Text> 
+              <Switch  value={fullList} onValueChange={checkValid2} color={'black'}/>
+            </View>
+          </View>}
+          
+          {isLoading && <ActivityIndicator size={50} color="white"/>}
+          {fullList ? (dataRec.map((item, index) => (
+            <View key={index} style={styles.item}>
+              <Text style={{fontWeight: 'bold',fontSize: 17, }}>ID: {item.id} - {item.name}</Text>
+              <Text >Quantity: {item.quantity}</Text>
+              <Text >Unit: {item.unit}</Text>
+              <Text >Price: {item.price.toFixed(2)}$</Text>
+              <Text 
+                style={{color: 'blue', fontWeight: 'bold'}}
+                onPress={() => Linking.openURL(`http:\\${item.website}`)}
+              >
+                Website: {item.website}
+              </Text>
+            </View>
+          ))) 
+          : ((dataRec && dataRec0) && 
+            <View style={styles.container}>
+            <View style={styles.item}>
+              <Text style={{fontWeight: 'bold',fontSize: 17,}}>ID: {dataRec0.id} - {dataRec0.name}</Text>
+              <Text>Quantity: {dataRec0.quantity}</Text>
+              <Text>Unit: {dataRec0.unit}</Text>
+              <Text>Price: {dataRec0.price.toFixed(2)}$</Text>
+              <Text
+                style={{ color: 'blue', fontWeight: 'bold' }}
+                onPress={() => Linking.openURL(`http:\\${dataRec0.website}`)}
+              >
+                Website: {dataRec0.website}
+              </Text>
+            </View>
+            </View>
+          )}
         </ScrollView>
     </View>
   );
